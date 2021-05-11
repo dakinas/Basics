ac0a8d68c9f6239746a7fdcf347eb482ecad7eb7
Alexey Dakin <82578120+dakinas@users.noreply.github.com>
2021-05-11 21:16:13 +0300

    Add files via upload

---
+++ Task 2.py
@@ -0,0 +1,4 @@
+def persona_func(first_name, last_name, bith_year, city, email, phone):
+        print(first_name, last_name, bith_year, city, email, phone)
+
+persona_func(first_name='Александр Сергеевич', last_name='Пушкин', bith_year=1799, city='Москва', email='alex@pushkin.his', phone='+7 001 000 00 01')
